d25983af6efb6edf7314d18b80003761fdcb6bd1
Samuel Clay <samuel@ofbrooklyn.com>
2019-05-11 17:39:54 -0400

    Just write the background without filter.

--- tracking_camera.py
+++ tracking_camera.py
@@ -17,11 +17,7 @@ def show_webcam(cam=None, mirror=True):
     img = cv2.resize(img, None, fx=0.5, fy=0.5)
     if not background_once:
         # background_once = True
-        background = img.copy()
-        output = img.copy()
-        cv2.rectangle(background, (0, 0), (background.shape[0]*2, background.shape[1]), (255, 255, 255), -1)
-        cv2.addWeighted(background, 0.25, output, 1 - 0.25, 0, output)
-        cv2.imwrite('background.jpg', output)
+        cv2.imwrite('background.jpg', img)
     
     return img
 
